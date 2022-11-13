5c19b82584892a53e674a0f6aa5231d8a359fae9
Sebastian Strojny <sebastian.strojny@gmail.com>
2022-11-13 10:46:34 +0100

    Inital commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+let button1 = document.querySelector(".js-photoSwitch");
+let photo = document.querySelector(".topBox__image");
+let photoButtonText = document.querySelector(".js-photoButtonText");
+
+button1.addEventListener("click", () => {
+
+    photo.classList.toggle("js-photoHide");
+
+    photoButtonText.innerText = photo.classList.contains("js-photoHide") ? "Show" : "Hide";
+
+});
+
+
+let button2 = document.querySelector(".js-darkModeSwitch");
+let body = document.querySelector(".js-body");
+let DarkModeButtonText = document.querySelector(".js-darkModeButtonText");
+let linkAboutMeDarkMode = document.querySelector(".js-linkAboutMeDarkMode");
+let linkPlanDarkMode = document.querySelector(".js-linkPlanDarkMode");
+let linkHobbyDarkMode = document.querySelector(".js-linkHobbyDarkMode");
+let linkContactDarkMode = document.querySelector(".js-linkContactDarkMode");
+let tableRowDarkMode = document.querySelector(".js-tableRowDarkMode");
+
+button2.addEventListener("click", () => {
+
+    body.classList.toggle("darkMode");
+
+    DarkModeButtonText.innerText = body.classList.contains("darkMode") ? "Light" : "Dark";
+
+    linkAboutMeDarkMode.classList.toggle("nav__linkDarkMode");
+    linkPlanDarkMode.classList.toggle("nav__linkDarkMode");
+    linkHobbyDarkMode.classList.toggle("nav__linkDarkMode");
+    linkContactDarkMode.classList.toggle("nav__linkDarkMode");
+
+    linkAboutMeDarkMode.classList.toggle("nav__linkDarkMode:visited");
+    linkPlanDarkMode.classList.toggle("nav__linkDarkMode:visited");
+    linkHobbyDarkMode.classList.toggle("nav__linkDarkMode:visited");
+    linkContactDarkMode.classList.toggle("nav__linkDarkMode:visited");
+
+    tableRowDarkMode.classList.toggle("table__rowDarkMode:hover");
+
+});
