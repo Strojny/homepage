9fa7cc0293e30f2ee6b52063d59874e6ae11aa59
Sebastian Strojny <sebastian.strojny@gmail.com>
2023-02-10 14:28:52 +0100

    Dark Mode into function

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 
         photo.classList.toggle("js-photoHide");
     }
-    
+
     const togglePhotoButtonText = () => {
         const photoButtonText = document.querySelector(".js-photoButtonText");
         const photo = document.querySelector(".topBox__image");
@@ -24,31 +24,51 @@
         button1.addEventListener("click", togglePhotoAndButtonClasses);
     }
 
-    const toggleBackground = () => {
-        const body = document.documentElement;
-        const tableDarkMode = document.querySelectorAll(".js-tableDarkMode");
-        const linkDarkMode = document.querySelectorAll(".js-linkDarkMode");
+    const DarkModeButtonInnerTextToggle = (body) => {
         const darkModeButtonText = document.querySelector(".js-darkModeButtonText");
 
+        darkModeButtonText.innerText = body.classList.contains("darkMode") ? "Light" : "Dark";
+    }
+
+    const darkMode = () => {
+        const button2 = document.querySelector(".js-darkModeSwitch");
+
+        button2.addEventListener("click", toggleBackground);
+    }
+
+    const bodyDarkModeToggle = (body) => {
+
         body.classList.toggle("darkMode");
+    }
+
+    const tableDarkModeToggle = () => {
+        const tableDarkMode = document.querySelectorAll(".js-tableDarkMode");
 
         tableDarkMode.forEach(tableRow => {
-
             tableRow.classList.toggle("table__DarkMode");
         });
+    }
+
+    const linkDarkModeToggle = () => {
+        const linkDarkMode = document.querySelectorAll(".js-linkDarkMode");
 
         linkDarkMode.forEach(nav__link => {
-
             nav__link.classList.toggle('nav__linkDarkMode');
         });
+    }
 
-        darkModeButtonText.innerText = body.classList.contains("darkMode") ? "Light" : "Dark";
+    const toggleBackground = () => {
+        const body = document.documentElement;
+
+        bodyDarkModeToggle(body);
+        DarkModeButtonInnerTextToggle(body);
+        tableDarkModeToggle();
+        linkDarkModeToggle();
     };
 
     const init = () => {
-        const button2 = document.querySelector(".js-darkModeSwitch");
 
-        button2.addEventListener("click", toggleBackground)
+        darkMode();
 
         photoHide();
     };
